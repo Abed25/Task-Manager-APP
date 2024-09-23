54507493c4b8773a9d93532ce7b655d58c115529
Abed25 <officialwekesa254@gmail.com>
2024-09-23 23:37:08 +0300

    modified tree stracture

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../css/home.css'
+
+function Home() {
+  
+  return (
+    <div>
+        <div classname="taskCont">
+          <h1>Task list</h1>
+        </div>
+        <div classname="adminCont">
+          <h1>Task manager</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Home
